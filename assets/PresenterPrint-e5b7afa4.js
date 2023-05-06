2a344d3360ee320c66bc76dcb49c16f6c21142f2
dmitriygrosh <dmitriy.grosh@gmail.com>
2023-05-06 03:15:41 +0300

    Updates

---
+++ assets/PresenterPrint-e5b7afa4.js
@@ -0,0 +1,17 @@
+import{d,i as _,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as o,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as k,m as N,p as P,q as S,_ as w}from"./index-d3e4517a.js";import{N as V}from"./NoteDisplay-f2b39796.js";const j={class:"m-4"},D={class:"mb-10"},H={class:"text-4xl font-bold mt-2"},L={class:"opacity-50"},T={class:"text-lg"},B={class:"font-bold flex gap-2"},C={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=d({__name:"PresenterPrint",setup(q){_(p),h(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),u({title:`Notes - ${m.title}`});const l=f(()=>y.slice(0,-1).map(s=>{var i;return(i=s.meta)==null?void 0:i.slide}).filter(s=>s!==void 0&&s.noteHTML!==""));return(s,i)=>(r(),n("div",{id:"page-root",style:x(a(S))},[t("div",j,[t("div",D,[t("h1",H,o(a(m).title),1),t("div",L,o(new Date().toLocaleString()),1)]),(r(!0),n(g,null,v(a(l),(e,c)=>(r(),n("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",T,[t("div",B,[t("div",C,o(e==null?void 0:e.no)+"/"+o(a(b)),1),k(" "+o(e==null?void 0:e.title)+" ",1),z])]),N(V,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),c<a(l).length-1?(r(),n("hr",F)):P("v-if",!0)]))),128))])],4))}}),J=w(M,[["__file","/Users/dmitriygrosh/Desktop/HolyJs Spring 2023/presentation/slidev/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{J as default};
